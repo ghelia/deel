787019bf5c42976861fbc88912b1a953e3511360
shi3z <shi3z@me.com>
2016-04-04 21:55:09 +0900

    Make getPretrainedModels.sh

---
+++ misc/getPretrainedModels.sh
@@ -0,0 +1,3 @@
+wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
+wget http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel
+wget https://github.com/BVLC/caffe/raw/master/python/caffe/imagenet/ilsvrc_2012_mean.npy
