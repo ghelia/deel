28c99448b6826dca2f6eb0c35a75bd070871579d
shi3z <shi3z@me.com>
2016-09-11 08:42:01 +0900

    ResNet152.py modified

--- deel/network/resnet152.py
+++ deel/network/resnet152.py
@@ -32,6 +32,14 @@ import random
 
 '''
 	ResNet152 by Caffenet ModelZoo
+
+	Please download .caffemodel and put on /misc
+
+	ResNet-152-model.caffemodel 
+	One Drive download https://onedrive.live.com/?authkey=%21AAFW2-FVoxeVRck&id=4006CBB8476FF777%2117887&cid=4006CBB8476FF777
+
+	https://github.com/KaimingHe/deep-residual-networks	
+
 '''
 class ResNet152(ImageNet):
 	def __init__(self,modelpath='ResNet-152-model.caffemodel',
@@ -39,12 +47,7 @@ class ResNet152(ImageNet):
 					labels='misc/labels.txt',in_size=224):
 		super(ResNet152,self).__init__('ResNet152',in_size)
 
-		if os.path.splitext(modelpath)[1]==".caffemodel":
-			self.func = LoadCaffeModel(modelpath)
-			self.model = self.func.copy()
-		else:
-			self.model = chainermodel.GoogLeNet()
-			cs.load_hdf5(modelpath,self.model)
+		self.func = LoadCaffeModel(modelpath)
 
 		xp = Deel.xp
 
@@ -55,28 +58,25 @@ class ResNet152(ImageNet):
 		ImageNet.mean_image[2] = 123
 		ImageNet.in_size = in_size
 
-		#print type(ImageNet.mean_image)
 		self.labels = np.loadtxt(labels, str, delimiter="\t")
 		self.batchsize = 1
 		self.x_batch = xp.ndarray((self.batchsize, 3, self.in_size, self.in_size), dtype=np.float32)
 
 		if Deel.gpu >=0:
-			self.model = self.model.to_gpu(Deel.gpu)
+			self.func = self.func.to_gpu(Deel.gpu)
 		self.optimizer = optimizers.MomentumSGD(lr=0.01,momentum=0.9)
 		#self.optimizer = optimizers.Adam()
-		#self.optimizer.setup(self.func)
-		self.optimizer.setup(self.model)
+		self.optimizer.setup(self.func)
 	def save(self,filename):
-		cs.save_hdf5(filename,self.model.to_cpu())
+		cs.save_hdf5(filename,self.func.to_cpu())
 
 	def forward(self,x,train=True):
-		y = self.model(x)
+		y, = self.func(inputs={'data': x}, outputs=['fc1000'],
+			train=train)
 		return y
 
 	def predict(self, x,train=False):
-		y, = self.func(inputs={'data': x}, outputs=['fc1000'],
-			train=train)
-		return F.softmax(y)
+		return F.softmax(self.forward(x,train=train))
 
 
 	def classify(self,x=None):
@@ -100,60 +100,6 @@ class ResNet152(ImageNet):
 
 		return t
 
-	def layerDim(self, layer='inception_5b/pool_proj'):
-		xp = Deel.xp
-		ImageNet.mean_image = np.ndarray((3, 256, 256), dtype=xp.float32)
-		ImageNet.mean_image[0] = 104
-		ImageNet.mean_image[1] = 117
-		ImageNet.mean_image[2] = 123
-
-		image = self.Input('deel.png').value
-		self.x_batch[0] = image
-		x_data = xp.asarray(self.x_batch)
-		x = chainer.Variable(x_data, volatile=True)
-
-		y, = self.func(inputs={'data': x}, outputs=[layer], train=False)
-
-		ImageNet.mean_image = np.ndarray((3, 256, 256), dtype=np.float32)
-		ImageNet.mean_image[0] = 104
-		ImageNet.mean_image[1] = 117
-		ImageNet.mean_image[2] = 123
-		return y.data.shape
-
-	def feature(self, x,layer=u'inception_5b/pool_proj'):
-		if x is None:
-			x=Tensor.context
-		if not isinstance(x,ImageTensor):
-			x=self.Input(x)
-
-		image = x.value
-
-		self.x_batch[0] = image
-		xp = Deel.xp
-		x_data = xp.asarray(self.x_batch)
-
-		if Deel.gpu >= 0:
-			x_data=cuda.to_gpu(x_data)
-		
-		x = chainer.Variable(x_data, volatile=True)
-		score, = self.func(inputs={'data': x}, outputs=[layer],
-			disable=['loss1/ave_pool', 'loss2/ave_pool'],
-			train=train)
-
-
-		if Deel.gpu >= 0:
-			score=cuda.to_cpu(score.data)
-			dim = getDim(score.shape)
-			score = score.reshape(dim)
-		else:
-			dim = getDim(score.data.shape)
-			score = score.data.reshape(dim)
-		
-		score = chainer.Variable(score*255.0, volatile=True)
-
-		t = ChainerTensor(score)
-		t.owner=self
-		t.use()
 
 	def batch_feature(self, x,t):
 		if x is None:
@@ -161,27 +107,18 @@ class ResNet152(ImageNet):
 
 		x = x.content
 		self.optimizer.zero_grads()
-		outputs = self.forward(x,train=True)
+		y = self.forward(x,train=True)
 
-		t = ChainerTensor(outputs[2])
+		t = ChainerTensor(y)
 		t.owner=self
 		t.use()
-
-		self.loss1=outputs[0]
-		self.loss2=outputs[1]
-		self.loss3=outputs[2]
-
 
 		return t
 
 	def backprop(self,t,x=None):
 		if x is None:
 			x=Tensor.context
-		loss1 = F.softmax_cross_entropy(self.loss1,t.content)
-		loss2 = F.softmax_cross_entropy(self.loss2,t.content)
-		loss3 = F.softmax_cross_entropy(self.loss3,t.content)
-
-		loss = 0.3*(loss1+loss2) +loss3
+		loss = F.softmax_cross_entropy(x.conent,t.content)
 
 		accuracy = F.accuracy(x.content,t.content)
 
