f389f6b92923bd040fe45b8791177d3806e71a91
shi3z <shi3z@me.com>
2016-09-24 08:11:24 +0900

    Support for chainer 1.16.0

--- deel/model/caffefunction.py
+++ deel/model/caffefunction.py
@@ -22,9 +22,9 @@ def _protobuf3():
 
 
 if _protobuf3():
-	from chainer.links.caffe import caffe_pb2 as caffe_pb
+	from chainer.links.caffe.protobuf3 import caffe_pb2 as caffe_pb
 	available = True
-
+	
 	try:
 		# This method is undocumented, but is required to read large size of
 		# model files when a user uses cpp-implementation.
@@ -35,7 +35,7 @@ if _protobuf3():
 
 elif sys.version_info < (3, 0, 0):
 	# caffe_pb2 does not support Py3
-	from chainer.links.caffe import caffe_pb2 as caffe_pb
+	from chainer.links.caffe.protobuf2 import caffe_pb2 as caffe_pb
 	available = True
 else:
 	available = False
@@ -208,7 +208,7 @@ class CaffeFunction(link.Chain):
 			   new_input_vars =[]
 			   for blob in input_vars:
 				   new_input_vars.append(
-				   		chainer.Variable(blob.data,volatile=volatile))
+						chainer.Variable(blob.data,volatile=volatile))
 			   input_vars = new_input_vars
 			   self.train=True
 			output_vars = func(*input_vars)
