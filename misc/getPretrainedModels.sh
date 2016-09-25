2467b3bdf69e0bbf738f97f14745e6c578811794
shi3z <shi3z@me.com>
2016-09-25 11:29:43 +0900

    hotfix for getPretraindModels

--- misc/getPretrainedModels.sh
+++ misc/getPretrainedModels.sh
@@ -6,3 +6,4 @@ wget https://dl.dropboxusercontent.com/u/38822310/age_net.caffemodel
 wget http://places.csail.mit.edu/model/googlenet_places205.tar.gz
 tar xzvf googlenet_places205.tar.gz
 mv googlenet_places205/googlelet_places205_train_iter_2400000.caffemodel .
+mv googlenet_places205/categoryIndex_places205.csv .
